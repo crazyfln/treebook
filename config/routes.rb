55c0d8dee59b184cb605c2647f5065499907d41c
crazyfln <jeff.wge@gmail.com>
2013-10-10 00:12:57 -0700

    new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Treebook::Application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
   resources :statuses
+  get 'home', to: 'statuses#index', as: :home
   root to: 'statuses#index'
 
   
