e1565ba8b5a0c776ce7623ce58dec088eb884948
crazyfln <jeff.wge@gmail.com>
2013-10-09 22:44:08 -0700

    Added test for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,14 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :first_name, :last_name, :profile_name
 
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates :profile_name, presence: true,
+                           uniqueness: true,
+                            format: { with: /\A[a-zA-Z0-9_-]+\Z/, message: "Please format correctly" }
+
   has_many :statuses
 
   def full_name
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "a user should enter a first name" do
+   	  user = User.new
+      assert !user.save
+      assert !user.errors[:first_name].empty?
+    end
+
+    test "a user should have a unique profile name" do
+    	user = User.new
+    	user.profile_name = users(:linan).profile_name
+ 
+    	assert !user.save
+
+    	assert !user.errors[:profile_name].empty?
+    end
+
+    test "a profile name should have no space" do
+    	user = User.new
+    	user.profile_name = "Wa Ha Ha"
+    	assert !user.save
+    	assert !user.errors[:profile_name].empty?
+    	assert user.errors[:profile_name].include?("Please format correctly")
+    end
 end
